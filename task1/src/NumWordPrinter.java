e4427f2a61f47bcc4a5edd31860e385097f7b36d
ayeshamashiat <ayesha.mashiat101@gmail.com>
2024-09-24 12:51:03 +0600

    add files

---
+++ task1/src/NumWordPrinter.java
@@ -0,0 +1,20 @@
+public class NumWordPrinter {
+    public void printWord(int n){
+        if(n % 2 == 0){
+            System.out.println("Even");
+        }
+        else if(n%11 == 0){
+            System.out.println("PrimeEleven");
+        }
+        else if(n%13 == 0){
+            System.out.println("LuckyThirteen");
+        }
+        else if(n%2 == 0 && n%11 == 0){
+            System.out.println("EvenPrimeEleven");
+        }
+        else if(n%2 == 0 && n%11 == 0 && n%13 == 0){
+            System.out.println("EvenPrimeLucky");
+        }
+
+    }
+}
