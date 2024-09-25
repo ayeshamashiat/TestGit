dde45cd2d8c26114eb3c179385d22aa866797d94
ayeshamashiat <ayesha.mashiat101@gmail.com>
2024-09-25 19:32:39 +0600

    added lines

--- task1/src/NumWordPrinter.java
+++ task1/src/NumWordPrinter.java
@@ -1,20 +1,24 @@
 public class NumWordPrinter {
-    public void printWord(int n){
-        if(n % 2 == 0){
-            System.out.println("Even");
+    public String printWord(int n){
+        if(n%2==0){
+            return "EVEN";
         }
-        else if(n%11 == 0){
-            System.out.println("PrimeEleven");
+        else if (n%11==0) {
+            return "PrimeEleven";
         }
-        else if(n%13 == 0){
-            System.out.println("LuckyThirteen");
+        else if (n%13==0){
+            return "LuckyThirteen";
         }
-        else if(n%2 == 0 && n%11 == 0){
-            System.out.println("EvenPrimeEleven");
+        else if(n%2 ==0 && n%11==0){
+            return "EvenPrimeEleven";
         }
-        else if(n%2 == 0 && n%11 == 0 && n%13 == 0){
-            System.out.println("EvenPrimeLucky");
+        else if(n%2==0 && n%13==0){
+            return "EvenLuckyThirteen";
+        }
+        else if(n%11==0 && n%2==0 && n%13==0){
+            return "EvenPrimeLucky";
         }
 
+        return "OddOneOut";
     }
 }
