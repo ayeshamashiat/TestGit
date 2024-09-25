45eb790cde3c30db27251867b616a0d731e90650
ayeshamashiat <ayesha.mashiat101@gmail.com>
2024-09-25 20:20:29 +0600

    added maxStack file

---
+++ task2/src/MaxStack.java
@@ -0,0 +1,11 @@
+import java.util.Stack;
+
+public class MaxStack {
+    Stack<Integer> mainStack;
+    Stack<Integer> maxStack;
+
+    public MaxStack() {
+        mainStack = new Stack<>();
+        maxStack = new Stack<>();
+    }
+}
